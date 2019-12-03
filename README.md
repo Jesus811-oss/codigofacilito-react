46d8aa7bbdc9b511f1a323b144efde2129fa8090
Jesus811-oss <lions01games@gmail.com>
2019-12-03 17:19:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-curses-0
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-curses-0)
---
+++ index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { render } from 'react-dom';
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: 'React'
+    }
+  }
+
+  render() {
+
+  }
+}
