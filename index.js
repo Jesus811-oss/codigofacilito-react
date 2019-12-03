8521d4d064fe2a8af5c1e6b2665ba6a32c5b3a0e
Jesus811-oss <lions01games@gmail.com>
2019-12-03 17:27:44 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import { render } from 'react-dom';
 
+function MyComponent() {
+  return <p>Sorry, ErrorType XD</p>;
+}
 
 class App extends Component {
   constructor(){
@@ -11,6 +14,12 @@ class App extends Component {
   }
 
   render() {
-
+      return (
+        <div>
+          <h1>Hello,React</h1>
+          <MyComponent/>
+        </div>);
   }
 }
+// Ejecutar React.js
+render(<App/>, document.getElementById('root'));
